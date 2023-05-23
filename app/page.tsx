696a95d434ed9eb1f145b9e6e05cb7c228ab9112
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-23 00:24:32 -0400

    Add navbar 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,10 @@
-import GlassPane from "@/components/GlassPane";
-import Navbar from "@/components/Navbar";
+import Navbar2 from "@/components/Navbar2";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <div className="bg-pattern h-screen w-screen">
-        <Navbar />
-      </div>
-      <div className="h-screen w-screen p-6">
-        <GlassPane className="w-full h-full flex items-center justify-center"></GlassPane>
-      </div>
+      <Hero />
     </main>
   );
-}
-
-{
-  /* <div className="flex flex-row">
-<div className="basis-full bg-primary">Navbar</div>
-</div>
-<div className="flex flex-row relative w-full h-screen mx-auto">
-<div className="basis-1/2 flex flex-col justify-center bg-tertiary items-center">
-  <button className="ml-200">TEST</button>
-</div>
-<div className="basis-1/2 bg-quinary">Picture here</div>
-</div>
-<div className="flex flex-row">
-<div className="basis-full bg-primary">YOW</div>
-</div> */
 }
