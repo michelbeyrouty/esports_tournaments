ac319055fba8c18f95eb48081b3c5c7434ae96c0
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-24 19:12:57 -0400

    Cleanup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,108 +6,46 @@ export default function Navbar() {
     <nav className="bg-blacky">
       <div className="mx-auto max-w-7xl">
         <div className="flex h-16 items-center justify-between">
-          <div className="flex items-center">
-            <img
-              className="h-8 w-8"
-              src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=500"
-              alt="Your Company"
-            />
-            <div className="ml-10 flex items-baseline space-x-4">
-              {navigation.map((nav) => (
-                <a
-                  href={nav.href}
-                  key={nav.name}
-                  className="text-white rounded-md px-3 py-2 text-sm font-medium"
-                  aria-current="page"
-                >
-                  {nav.name}
-                </a>
-              ))}
-            </div>
-          </div>
+          <NavItems />
           <Profile />
         </div>
       </div>
-
-      {/* <div className="md:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            <a
-              href="#"
-              className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-              aria-current="page"
-            >
-              Dashboard
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Team
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Projects
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Calendar
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Reports
-            </a>
-          </div>
-          <div className="border-t border-gray-700 pb-3 pt-4">
-            <div className="flex items-center px-5">
-              <div className="flex-shrink-0">
-                <img
-                  className="h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </div>
-              <div className="ml-3">
-                <div className="text-base font-medium leading-none text-white">
-                  Tom Cook
-                </div>
-                <div className="text-sm font-medium leading-none text-gray-400">
-                  tom@example.com
-                </div>
-              </div>
-              <button
-                type="button"
-                className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-              </button>
-            </div>
-            <div className="mt-3 space-y-1 px-2">
-              <a
-                href="#"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >
-                Your Profile
-              </a>
-              <a
-                href="#"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >
-                Settings
-              </a>
-              <a
-                href="#"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >
-                Sign out
-              </a>
-            </div>
-          </div>
-        </div> */}
     </nav>
+  );
+}
+
+function NavItems() {
+  return (
+    <div className="flex items-center">
+      <Logo />
+      <div className="ml-10 flex items-baseline space-x-4">
+        {navigation.map((nav) => (
+          <a
+            href={nav.href}
+            key={nav.name}
+            className="text-white rounded-md px-3 py-2 text-sm font-medium"
+            aria-current="page"
+          >
+            {nav.name}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <a
+      href="/"
+      className="text-white rounded-md px-3 py-2 text-sm font-medium"
+      aria-current="page"
+    >
+      <img
+        className="h-8 w-8"
+        src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=500"
+        alt="Your Company"
+      />
+    </a>
   );
 }
