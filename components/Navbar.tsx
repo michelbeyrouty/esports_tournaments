7e7c7ce7a24738da61823993171303bf5470a3e7
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-24 18:32:05 -0400

    Cleanup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,7 @@
-"use client";
-import { useState } from "react";
-
-const navigation = [
-  { name: "About us", href: "#about" },
-  { name: "Tournaments", href: "/tournaments" },
-  { name: "Login", href: "/login" },
-];
+import Profile from "./profile";
+const navigation = [{ name: "Tournaments", href: "/tournaments" }];
 
 export default function Navbar() {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
   return (
     <div className="min-h-full">
       <nav className="bg-blacky">
@@ -25,120 +17,20 @@ export default function Navbar() {
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
-                  <a
-                    href="#"
-                    className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                    aria-current="page"
-                  >
-                    Dashboard
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    Team
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    Projects
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    Calendar
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    Reports
-                  </a>
+                  {navigation.map((nav) => (
+                    <a
+                      href={nav.href}
+                      key={nav.name}
+                      className="text-white rounded-md px-3 py-2 text-sm font-medium"
+                      aria-current="page"
+                    >
+                      {nav.name}
+                    </a>
+                  ))}
                 </div>
               </div>
             </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center md:ml-6">
-                <button
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                    />
-                  </svg>
-                </button>
-
-                <div className="relative ml-3">
-                  <div>
-                    <button
-                      type="button"
-                      className="flex max-w-xs items-center rounded-full bg-blacky text-sm"
-                      id="user-menu-button"
-                      aria-expanded="false"
-                      aria-haspopup="true"
-                    >
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                        onClick={() => setMenuOpen(!isMenuOpen)}
-                      />
-                    </button>
-                  </div>
-
-                  {isMenuOpen ? (
-                    <div
-                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                      role="menu"
-                      aria-orientation="vertical"
-                      aria-labelledby="user-menu-button"
-                    >
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700"
-                        role="menuitem"
-                        id="user-menu-item-0"
-                      >
-                        Your Profile
-                      </a>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700"
-                        role="menuitem"
-                        id="user-menu-item-1"
-                      >
-                        Settings
-                      </a>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700"
-                        role="menuitem"
-                        id="user-menu-item-2"
-                      >
-                        Sign out
-                      </a>
-                    </div>
-                  ) : (
-                    <div />
-                  )}
-                </div>
-              </div>
-            </div>
+            <Profile />
             <div className="-mr-2 flex md:hidden">
               <button
                 type="button"
@@ -180,7 +72,7 @@ export default function Navbar() {
           </div>
         </div>
 
-        <div className="md:hidden" id="mobile-menu">
+        {/* <div className="md:hidden" id="mobile-menu">
           <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
             <a
               href="#"
@@ -273,7 +165,7 @@ export default function Navbar() {
               </a>
             </div>
           </div>
-        </div>
+        </div> */}
       </nav>
     </div>
   );
