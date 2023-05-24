d58a4bf23518a61b586a07d0fe48a988a17b98d5
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-24 18:40:19 -0400

    Cleanup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,7 +38,6 @@ export default function Navbar() {
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
-                <span className="sr-only">Open main menu</span>
                 <svg
                   className="block h-6 w-6"
                   fill="none"
@@ -127,7 +126,6 @@ export default function Navbar() {
                 type="button"
                 className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
-                <span className="sr-only">View notifications</span>
                 <svg
                   className="h-6 w-6"
                   fill="none"
--- components/profile.tsx
+++ components/profile.tsx
@@ -16,7 +16,6 @@ export default function Profile() {
               aria-expanded="false"
               aria-haspopup="true"
             >
-              <span className="sr-only">Open user menu</span>
               <img
                 className="h-8 w-8 rounded-full"
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
