0378febcbb9259b84b89f84b88341eeb8336dcc6
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-24 18:45:50 -0400

    Cleanup

--- components/profile.tsx
+++ components/profile.tsx
@@ -27,13 +27,22 @@ export default function Profile() {
               className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
               role="menu"
               aria-orientation="vertical"
-              aria-labelledby="user-menu-button"
             >
               <a
                 href="/login"
                 className="block px-4 py-2 text-sm text-gray-700"
-                role="menuitem"
-                id="user-menu-item-2"
+              >
+                Your profile
+              </a>
+              <a
+                href="/login"
+                className="block px-4 py-2 text-sm text-gray-700"
+              >
+                Settings
+              </a>
+              <a
+                href="/login"
+                className="block px-4 py-2 text-sm text-gray-700"
               >
                 Log in
               </a>
