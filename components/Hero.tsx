6c2ca4a2e878002c09192e11bb559319d8de6376
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-24 12:41:23 -0400

    Add navbar 1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="bg-white">
+    <div className="bg-bg">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
           className="flex items-center justify-between p-6 lg:px-8"
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       fontFamily: {
         'press-start': ['"Press Start 2P"', 'cursive'],
       },
+      colors: {
+        greeny: "#34d399",
+        bg: "rgb(24 24 27)"
+      },
       backgroundImage: {
         "pattern": "url('/bg.jpg')",
       },
