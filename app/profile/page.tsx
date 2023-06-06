60423bdaf2f4705541292746edde39b52f9528d1
michelbeyrouty <michelbeyrouty@gmail.com>
2023-06-06 17:41:29 -0400

    Clean up

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,9 @@
 export default function UserProfile() {
-  return <div> Under Construction 🏗️</div>;
+  return (
+    <div className="h-screen grid grid-cols-1 place-items-center">
+      <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
+        User Profile 🏗️
+      </h1>
+    </div>
+  );
 }
