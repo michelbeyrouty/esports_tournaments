dfb1723707693529e7fe169b897951921c3c73f1
michelbeyrouty <michelbeyrouty@gmail.com>
2023-07-04 00:48:21 -0400

    Cleanup

--- components/BannerSection.tsx
+++ components/BannerSection.tsx
@@ -4,13 +4,13 @@ import Card from "./Card";
 
 export default function BannerSection() {
   return (
-    <div className="w-screen h-96">
-      <Card className="m-[10%] h-[60%] w-[80%] bg-greeny flex sm:flex-row flex-col items-center justify-between">
+    <div className="w-screen h-64">
+      <Card className="m-[10%] h-[50%] w-[80%] bg-greeny flex sm:flex-row flex-col items-center justify-between">
         <div className="ml-0 sm:ml-[5%] mt-[5%] sm:mt-0 sm:w-auto w-72">
-          <h2 className="text-3xl font-semibold tracking-tight italic text-center">
+          <h2 className="sm:text-3xl text-xl font-semibold tracking-tight italic text-center">
             Play your favorite games
           </h2>
-          <h2 className="text-3xl font-semibold tracking-tight italic text-center">
+          <h2 className="sm:text-3xl text-xl font-semibold tracking-tight italic text-center">
             & win real money
           </h2>
         </div>
