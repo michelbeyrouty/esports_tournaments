04bc60723c7cc65de1f49f476989170ad8c79900
michelbeyrouty <michelbeyrouty@gmail.com>
2023-07-04 01:20:18 -0400

    Cleanup

--- components/BannerSection.tsx
+++ components/BannerSection.tsx
@@ -4,9 +4,9 @@ import Card from "./Card";
 
 export default function BannerSection() {
   return (
-    <div className="w-screen h-64">
-      <Card className="m-[10%] h-[50%] w-[80%] bg-greeny flex sm:flex-row flex-col items-center justify-between">
-        <div className="ml-0 sm:ml-[5%] mt-[5%] sm:mt-0 sm:w-auto w-72">
+    <div className="w-screen h-96 flex flex-col justify-center">
+      <Card className="m-[10%] w-[80%] h-40 bg-greeny flex sm:flex-row flex-col items-center justify-between">
+        <div className="ml-0 sm:ml-[5%] sm:mt-0 mt-3 sm:w-auto w-72">
           <h2 className="sm:text-3xl text-xl font-semibold tracking-tight italic text-center">
             Play your favorite games
           </h2>
@@ -14,7 +14,7 @@ export default function BannerSection() {
             & win real money
           </h2>
         </div>
-        <Link href="#tournaments" passHref>
+        <Link href="#tournaments" passHref className="sm:h-10 h-15">
           <Button intent="secondary" className="sm:mr-5 mb-5">
             {"Find a tournament"}
           </Button>
